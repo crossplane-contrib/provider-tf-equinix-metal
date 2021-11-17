540c55c8012f29a8306cb9a63a4054c79e10e2d1
Marques Johansson <mjohansson@equinix.com>
2021-11-17 16:08:27 -0500

    set the externalname of project resources to the project id
    Project names are not uniquely constrained in the EM API.
    
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- config/project/config.go
+++ config/project/config.go
@@ -5,6 +5,7 @@ import "github.com/crossplane-contrib/terrajet/pkg/config"
 // Customize the device group with references to other resources
 func Customize(p *config.Provider) {
 	p.AddResourceConfigurator("metal_project", func(r *config.Resource) {
+		r.ExternalName = config.IdentifierFromProvider
 		r.Group = "project"
 	})
 }
